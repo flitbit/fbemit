5911a5778ddd59c7f008bc2005ef93786fbfd1c4
= <=>
2013-02-04 08:53:55 -0700

    Added factory that creates auto-implemented types. Added wireup.

---
+++ FlitBit.Emit/AutoImplementedTypeFactory.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Concurrent;
+using FlitBit.Core;
+using FlitBit.Core.Factory;
+using FlitBit.Emit.Meta;
+
+namespace FlitBit.Emit
+{
+	/// <summary>
+	/// Factory capable of constructing auto-implemented types.
+	/// </summary>
+	public sealed class AutoImplementedTypeFactory : IFactory
+	{
+		struct TypeRecord
+		{
+			public Type TargetType;
+			public Delegate Factory;
+
+			internal object CreateInstance()
+			{
+				return (TargetType == null) ? Factory.DynamicInvoke(null) : Activator.CreateInstance(TargetType);
+			}
+		}
+		ConcurrentDictionary<object, TypeRecord> _emittedTypes = new ConcurrentDictionary<object, TypeRecord>();
+
+		/// <summary>
+		/// Creates a new instance of type T.
+		/// </summary>
+		/// <typeparam name="T">type T</typeparam>
+		/// <returns></returns>
+		public T CreateInstance<T>()
+		{
+			if (typeof(T).IsAbstract && typeof(T).IsDefined(typeof(AutoImplementedAttribute), true))
+			{
+				return CreateAutoImplemented<T>();
+			}
+			return (T)Activator.CreateInstance<T>();
+		}
+
+		private T CreateAutoImplemented<T>()
+		{
+			var type = typeof(T);
+			var typeLock = type.GetLockForType();
+			TypeRecord rec;
+			if (!_emittedTypes.TryGetValue(typeLock, out rec))
+			{
+				var emitted = false;
+				foreach (AutoImplementedAttribute attr in type.GetCustomAttributes(typeof(AutoImplementedAttribute), false))
+				{
+					if (attr.GetImplementation<T>((impl, factory) =>
+					{
+						// use the implementation type if provided
+						rec = new TypeRecord { TargetType = impl, Factory = factory };
+						_emittedTypes.TryAdd(typeLock, rec);
+					}))
+					{
+						emitted = true;
+						break;
+					}
+				}
+				if (!emitted)
+				{
+					throw new InvalidOperationException(String.Concat("Unable to access implementation of ", type.GetReadableFullName()));
+				}
+			}
+			return (T)rec.CreateInstance();
+		}
+	}
+}
---
+++ FlitBit.Emit/Wireup.cs
@@ -0,0 +1,24 @@
+﻿using FlitBit.Core;
+using FlitBit.Wireup;
+using FlitBit.Wireup.Meta;
+
+[assembly: Wireup(WireupBehaviors.DeclarationsOnly, typeof(FlitBit.Emit.WireupThisAssembly))]
+
+namespace FlitBit.Emit
+{
+	/// <summary>
+	/// Wires this assembly.
+	/// </summary>
+	public sealed class WireupThisAssembly: WireupCommand
+	{
+		/// <summary>
+		/// Performs the wireup.
+		/// </summary>
+		/// <param name="coordinator"></param>
+		protected override void PerformWireup(IWireupCoordinator coordinator)
+		{
+			// Register auto-implemented factory as the global; enables emitted types.
+			FactoryFactory.Instance = new AutoImplementedTypeFactory();
+		}
+	}
+}
